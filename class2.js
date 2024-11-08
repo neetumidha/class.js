de0b8156e4b2cca79d5919c654d2a8bd98f91743
neetu <neetu@gmail.com>
2024-11-08 16:37:50 +0530

    class

---
+++ class2.js
@@ -0,0 +1,15 @@
+// var a = 10
+// var b = 20
+// console.log(a+b)
+
+// var a = 10
+// console.log('neetu')
+
+// var $a = 10
+// console.log($a)
+
+// var _a = 20
+// console.log(_a)
+
+var a = 'neetu'
+console.log (a)
