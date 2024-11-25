7d2652dbc300caf03dee72662816e34dfbac86c8
neetu <neetu@gmail.com>
2024-11-25 15:59:07 +0530

    netu

---
+++ class9.js
@@ -0,0 +1,15 @@
+// let a = 13
+// if(a%2==0){
+//     console.log('this number is even')
+// }
+// else{
+//  console.log("this number is odd")
+// }
+
+let a = 14
+if(a%2==0){
+    console.log('this number is even')
+}
+else{
+    console.log("this number is odd")
+}
