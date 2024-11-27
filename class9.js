2c704c840f3310738ae4111a52bbd4a1bcb50cca
neetu <neetu@gmail.com>
2024-11-27 17:01:43 +0530

    arrayclass

--- class9.js
+++ class9.js
@@ -6,10 +6,21 @@
 //  console.log("this number is odd")
 // }
 
-let a = 14
-if(a%2==0){
-    console.log('this number is even')
+//  let a = 14
+//    if(a%2==0){
+//  console.log('this number is even')
+// }
+//  else{
+//      console.log("this number is odd")
+//  }
+
+ let a = 2000
+   if(a%4==0){
+ console.log('this year is leap ')
 }
-else{
-    console.log("this number is odd")
-}
+ else{
+     console.log("this isn't leap year ")
+ }
+
+ 
+ 
